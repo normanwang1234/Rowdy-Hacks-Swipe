c5620d5f6d44628da4ec5e801d4d465fa7abf17f
Norman Wang <normanwang@utexas.edu>
2020-03-29 04:23:50 -0500

    Updates

---
+++ precache-manifest.2da24539078557f3b5b7e06621c1c329.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "febf5cf87583e84a47b3",
+    "url": "/swipe/static/css/main.2103fb67.chunk.css"
+  },
+  {
+    "revision": "febf5cf87583e84a47b3",
+    "url": "/swipe/static/js/main.163217c9.chunk.js"
+  },
+  {
+    "revision": "443c91e4a674c8891f61",
+    "url": "/swipe/static/js/runtime~main.1e69976d.js"
+  },
+  {
+    "revision": "a40c45509e1706de4205",
+    "url": "/swipe/static/js/2.34d403db.chunk.js"
+  },
+  {
+    "revision": "81d7c3dfa3a5838bc1a21401331edcf0",
+    "url": "/swipe/index.html"
+  }
+];
